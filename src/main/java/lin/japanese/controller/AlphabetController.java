eaffc949d33c4397f786f98a46d1c899c5a698e4
JiaHongLin <joe830507@gmail.com>
2023-05-21 00:03:18 +0800

    addAlphabet

--- src/main/java/lin/japanese/controller/AlphabetController.java
+++ src/main/java/lin/japanese/controller/AlphabetController.java
@@ -11,24 +11,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lin.japanese.dto.AlphabetAddDto;
+import lin.japanese.dto.ResponseWrapper;
 import lin.japanese.service.AlphabetService;
 
 @RestController
 @RequestMapping(value = "/api/alphabet")
-public class AlphabetController {
+public class AlphabetController extends BaseController {
 
 	@Autowired
 	private AlphabetService alphabetService;
 
 	@PostMapping
-	public ResponseEntity<String> addWord(@RequestBody String input){
-		return null;
+	public ResponseEntity<ResponseWrapper> addAlphabet(@RequestBody AlphabetAddDto addDto) {
+		alphabetService.addAlphabet(addDto);
+		return ResponseEntity.ok(defaultOk());
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<Map<String,String>> getWords(){
+	public ResponseEntity<ResponseWrapper> getAlphabets() {
 		Map<String, String> resultMap = alphabetService.getAlphabets().stream()
 				.collect(Collectors.toMap(item -> item.getJapanese(), item -> item.getRoma()));
-		return ResponseEntity.ok(resultMap);
+		return ResponseEntity.ok(defaultOkWithResBody(resultMap));
 	}
 }
