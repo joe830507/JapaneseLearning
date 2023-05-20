6f994c784dd9ddf596567618722df759471fbb56
JiaHongLin <joe830507@gmail.com>
2023-05-21 00:02:55 +0800

    add method - addAlphabet

--- src/main/java/lin/japanese/service/AlphabetService.java
+++ src/main/java/lin/japanese/service/AlphabetService.java
@@ -3,8 +3,9 @@ package lin.japanese.service;
 import java.util.List;
 
 import lin.japanese.domain.Alphabet;
+import lin.japanese.dto.AlphabetAddDto;
 
 public interface AlphabetService {
-
+	void addAlphabet(AlphabetAddDto addDto);
 	public List<Alphabet> getAlphabets();
 }
