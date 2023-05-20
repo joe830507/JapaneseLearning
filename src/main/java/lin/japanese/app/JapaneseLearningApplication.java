f58074267b403e578490992e5a77070027700a55
JiaHongLin <joe830507@gmail.com>
2023-05-21 00:14:43 +0800

    add @EntityScan to the starter otherwise entities won't be managed by the entity manager.

--- src/main/java/lin/japanese/app/JapaneseLearningApplication.java
+++ src/main/java/lin/japanese/app/JapaneseLearningApplication.java
@@ -2,10 +2,12 @@ package lin.japanese.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "lin.japanese")
 @EnableJpaRepositories(basePackages = "lin.japanese.repository")
+@EntityScan(basePackages = "lin.japanese.domain")
 public class JapaneseLearningApplication {
 
 	public static void main(String[] args) {
