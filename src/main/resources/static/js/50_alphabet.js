7ab5055a6e55ddea689756782d92159e842d7d3d
Joe <joe830507@gmail.com>
2023-05-24 22:36:46 +0800

    add 50 alphabets

---
+++ src/main/resources/static/js/50_alphabet.js
@@ -0,0 +1,186 @@
+const _alphabets = [
+	{
+		japanese: 'あ',
+		roma: 'a'
+	},
+	{
+		japanese: 'い',
+		roma: 'i'
+	},
+	{
+		japanese: 'う',
+		roma: 'u'
+	},
+		{
+		japanese: 'え',
+		roma: 'e'
+	},
+	{
+		japanese: 'お',
+		roma: 'o'
+	},	
+	{
+		japanese: 'か',
+		roma: 'ka'
+	},	
+	{
+		japanese: 'き',
+		roma: 'ki'
+	},	
+	{
+		japanese: 'く',
+		roma: 'ku'
+	},	
+	{
+		japanese: 'け',
+		roma: 'ke'
+	},	
+	{
+		japanese: 'こ',
+		roma: 'ko'
+	},	
+	{
+		japanese: 'さ',
+		roma: 'sa'
+	},
+	{
+		japanese: 'し',
+		roma: 'si'
+	},
+	{
+		japanese: 'す',
+		roma: 'su'
+	},
+	{
+		japanese: 'せ',
+		roma: 'se'
+	},
+	{
+		japanese: 'そ',
+		roma: 'so'
+	},
+	{
+		japanese: 'た',
+		roma: 'ta'
+	},
+	{
+		japanese: 'ち',
+		roma: 'ti'
+	},
+	{
+		japanese: 'つ',
+		roma: 'tu'
+	},
+	{
+		japanese: 'て',
+		roma: 'te'
+	},
+	{
+		japanese: 'と',
+		roma: 'to'
+	},
+	{
+		japanese: 'な',
+		roma: 'na'
+	},
+	{
+		japanese: 'に',
+		roma: 'ni'
+	},
+	{
+		japanese: 'ぬ',
+		roma: 'nu'
+	},
+	{
+		japanese: 'ね',
+		roma: 'ne'
+	},
+	{
+		japanese: 'の',
+		roma: 'no'
+	},
+	{
+		japanese: 'は',
+		roma: 'ha'
+	},
+	{
+		japanese: 'ひ',
+		roma: 'hi'
+	},
+	{
+		japanese: 'ふ',
+		roma: 'hu'
+	},
+	{
+		japanese: 'へ',
+		roma: 'he'
+	},
+	{
+		japanese: 'ほ',
+		roma: 'ho'
+	},
+	{
+		japanese: 'ま',
+		roma: 'ma'
+	},
+	{
+		japanese: 'み',
+		roma: 'mi'
+	},
+	{
+		japanese: 'む',
+		roma: 'mu'
+	},
+	{
+		japanese: 'め',
+		roma: 'me'
+	},
+	{
+		japanese: 'も',
+		roma: 'mo'
+	},
+	{
+		japanese: 'や',
+		roma: 'ya'
+	},
+	{
+		japanese: 'ゆ',
+		roma: 'yu'
+	},
+	{
+		japanese: 'よ',
+		roma: 'yo'
+	},
+	{
+		japanese: 'ら',
+		roma: 'ra'
+	},
+	{
+		japanese: 'り',
+		roma: 'ri'
+	},
+	{
+		japanese: 'る',
+		roma: 'ru'
+	},
+	{
+		japanese: 'れ',
+		roma: 're'
+	},
+	{
+		japanese: 'ろ',
+		roma: 'ro'
+	},
+	{
+		japanese: 'わ',
+		roma: 'wa'
+	},
+	{
+		japanese: 'を',
+		roma: 'wo'
+	},
+	{
+		japanese: 'ん',
+		roma: 'n'
+	},
+]
