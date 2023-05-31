909e5c15abf9a325f25babbf219395dbf1e683a9
JiaHongLin <joe830507@gmail.com>
2023-05-31 11:12:42 +0800

    Update AlphabetQueryDto.java
    validation doesn't work when I use get method

--- src/main/java/lin/japanese/dto/AlphabetQueryDto.java
+++ src/main/java/lin/japanese/dto/AlphabetQueryDto.java
@@ -1,11 +1,8 @@
 package lin.japanese.dto;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.Data;
 
 @Data
 public class AlphabetQueryDto {
-	@NotBlank(message = "Category should not be blank")
 	private String category;
 }
