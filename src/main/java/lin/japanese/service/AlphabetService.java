2d6233d5a816a3c5cb4b2d35c02c0050e4288eb4
JiaHongLin <joe830507@gmail.com>
2023-05-31 11:12:50 +0800

    Update AlphabetService.java
    remove post method( not necessary)

--- src/main/java/lin/japanese/service/AlphabetService.java
+++ src/main/java/lin/japanese/service/AlphabetService.java
@@ -3,11 +3,8 @@ package lin.japanese.service;
 import java.util.List;
 
 import lin.japanese.domain.Alphabet;
-import lin.japanese.dto.AlphabetAddDto;
 import lin.japanese.dto.AlphabetQueryDto;
 
 public interface AlphabetService {
-	void addAlphabet(AlphabetAddDto addDto);
-
 	public List<Alphabet> getAlphabets(AlphabetQueryDto queryDto);
 }
